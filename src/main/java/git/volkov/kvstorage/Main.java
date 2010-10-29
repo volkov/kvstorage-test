060423b15e8c3f54b1f1977225ae4b328555f431
Sergey Volkov <sergey.volkov.a@gmail.com>
2010-10-30 00:38:29 +0400

    add maven support

---
+++ src/main/java/git/volkov/kvstorage/Main.java
@@ -0,0 +1,11 @@
+package git.volkov.kvstorage;
+/**
+ * Main class of test project.
+ * @author Sergey Volkov
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("do nothing");
+	}
+}
